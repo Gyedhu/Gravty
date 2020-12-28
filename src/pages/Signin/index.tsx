f6c32ef1a00242cd8ff086499d1b39424e52ca56
Yedhu <gyedhumohanan@gmail.com>
2020-12-28 20:16:19 +0530

    Added signin page validations

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,19 +1,22 @@
-import { useState } from "react";
+import { FC, useState } from "react";
 import { CheckBox } from "../../components";
 import Input from "../../components/Input";
 import { FormContainer } from "../../container";
 import { useForm } from "react-hook-form";
 
-interface SubmitData {
+interface UserData {
   email: string;
   password: string;
 };
+
+const ErrorText: FC = ({ children }) =>
+  <span style={{ color: "tomato" }}>{children}</span>
 
 const Signin = () => {
 
   // react-hook-form
   // Get register, errors, handleSubmit
-  const { register, errors, handleSubmit } = useForm();
+  const { register, errors, handleSubmit } = useForm<UserData>();
 
   // ShowPassword for set password visibility
   const [showPassword, setShowPassword] = useState(false);
@@ -23,7 +26,7 @@ const Signin = () => {
     setShowPassword(prev => !prev);
 
   // Submit form data
-  const submitForm = (onValid: SubmitData) => {
+  const submitForm = (onValid: UserData) => {
     console.log(onValid);
   }
 
@@ -43,6 +46,12 @@ const Signin = () => {
       ref={register({ required: true })}
       type="email"
     />
+    {
+      // Email is required Error
+      errors.email && errors.email.type === "required" && (
+        <ErrorText>Please enter your email!</ErrorText>
+      )
+    }
 
     {/* Password field */}
     <Input
@@ -51,6 +60,12 @@ const Signin = () => {
       ref={register({ required: true })}
       type={showPassword ? "text" : "password"}
     />
+    {
+      // Password is required Error
+      errors.password && errors.password.type === "required" && (
+        <ErrorText>Please enter your password!</ErrorText>
+      )
+    }
 
     {/* Checkbox for toggle password visibility */}
     <CheckBox onChange={togglePassword} title="Show Password" />
