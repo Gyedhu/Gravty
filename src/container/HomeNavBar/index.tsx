53577b13cee3731619bb01b9a1061d7c6b2890db
Yedhu <gyedhumohanan@gmail.com>
2020-12-31 21:06:20 +0530

    Removed bugs

--- src/container/HomeNavBar/index.tsx
+++ src/container/HomeNavBar/index.tsx
@@ -1,15 +1,17 @@
 import React from "react";
-import { Button } from "../../components";
+import { Button, FlexView } from "../../components";
 import ButtonSet from "../ButtonSet";
 
 
 const HomeNavBar = () => {
   return <ButtonSet>
-    <Button className="ri-add-line" to="/write/question" />
-    <Button className="ri-message-line" />
-    <Button className="ri-search-2-line" />
-    <Button className="ri-upload-cloud-2-line" />
-    <Button className="ri-team-line" />
+    <FlexView gap="10px" mobile>
+      <Button className="ri-add-line" to="/write/question" />
+      <Button className="ri-message-line" />
+      <Button className="ri-search-2-line" />
+      <Button className="ri-upload-cloud-2-line" />
+      <Button className="ri-team-line" />
+    </FlexView>
   </ButtonSet>
 }
 
