4310911e759d893a59908404dc2db9fe20ea1d94
Yedhu <gyedhumohanan@gmail.com>
2020-12-31 21:39:37 +0530

    Removed all state declaration and functions from <PageEditButtons />

--- src/pages/PageEditor/index.tsx
+++ src/pages/PageEditor/index.tsx
@@ -1,60 +1,47 @@
 import React from "react";
 import { Button, FlexView, FloatingBox, TextArea } from "../../components";
-import {  PageEditButtons, Paragraph } from "../../container";
+import { PageEditButtons, Paragraph } from "../../container";
 import Header from "../../container/Header";
 import View from "../../container/View";
 
 const PageEditor = () => {
 
-  const [selectElement, setSelectElement] = React.useState(false);
-  const [write, setWrite] = React.useState(false);
-
-  const gotoWrite = () => {
-    setWrite(prev => !prev)
-    setSelectElement(false);
-  };
-
-
   return <View>
-
-    <Header title="Editor" subTitle="Write your page"  /> 
+    <Header title="Editor" subTitle="Write your page" />
     <PageEditButtons />
 
-    <FloatingBox side="center" active={selectElement}>
+    <FloatingBox side="center" active={true}>
       <View>
         <FlexView direction="column" gap="30px" paddingHorizontal="20px" paddingVertical="20px" popup radius="10px" shadow>
+
           <FlexView justify="space-between">
             <Paragraph
               header="Elements"
               content="Select one you want to add"
             />
-
-            <Button className="ri-close-circle-line" onClick={() => setSelectElement(false)} />
+            <Button className="ri-close-circle-line" />
           </FlexView>
 
           <FlexView direction="column" gap="10px" maxHeight="250px">
-            <Button border onClick={gotoWrite} rounded title="Header" width="fit-content" />
-            <Button border onClick={gotoWrite} rounded title="Pagragraph" width="fit-content" />
-            <Button border onClick={gotoWrite} rounded title="Paragraph with header" width="fit-content" />
-            <Button border onClick={gotoWrite} rounded title="List" width="fit-content" />
-            <Button border onClick={gotoWrite} rounded title="Image" width="fit-content" />
-            <Button border onClick={gotoWrite} rounded title="Audio" width="fit-content" />
+            <Button border rounded title="Header" width="fit-content" />
+            <Button border rounded title="Pagragraph" width="fit-content" />
+            <Button border rounded title="Paragraph with header" width="fit-content" />
+            <Button border rounded title="List" width="fit-content" />
+            <Button border rounded title="Image" width="fit-content" />
+            <Button border rounded title="Audio" width="fit-content" />
           </FlexView>
         </FlexView>
       </View>
     </FloatingBox >
 
-    <FloatingBox side="bottom" active={write}>
+    <FloatingBox side="bottom" active={true}>
       <View>
         <TextArea placeholder="Write your content" />
         <FlexView>
           <Button background fit title="Finished" />
           <Button className="ri-attachment-2" title="Attach" />
           <Button className="ri-delete-bin-line" title="Clear" />
-          <Button
-            className="ri-close-circle-line"
-            onClick={gotoWrite}
-          />
+          <Button className="ri-close-circle-line" />
         </FlexView>
       </View>
     </FloatingBox>
