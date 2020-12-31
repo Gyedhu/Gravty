80cd9e66e18ce8fa0b311276b3113c141ffa21fe
Yedhu <gyedhumohanan@gmail.com>
2020-12-31 21:05:11 +0530

    Added <ButtonSet  /> in <HomeNavBar />

--- src/container/HomeNavBar/index.tsx
+++ src/container/HomeNavBar/index.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { FlexView, Button } from "../../components";
+import { Button } from "../../components";
+import ButtonSet from "../ButtonSet";
 
 
 const HomeNavBar = () => {
-  return <FlexView gap="10px" mobile paddingHorizontal="15px" paddingVertical="15px" radius="10px" shadow>
+  return <ButtonSet>
     <Button className="ri-add-line" to="/write/question" />
     <Button className="ri-message-line" />
     <Button className="ri-search-2-line" />
     <Button className="ri-upload-cloud-2-line" />
     <Button className="ri-team-line" />
-  </FlexView>
+  </ButtonSet>
 }
 
 export default HomeNavBar;
